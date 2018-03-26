b5fb035e834eb36bee47bd1a903d3396114c4424
008rajat <mail008rajat@gmail.com>
2018-03-26 21:51:22 +0530

    Create sample.c
    How to use time() function.

---
+++ sample.c
@@ -0,0 +1,25 @@
+#include<unistd.h>
+#include<time.h>
+#include<stdio.h>
+#include<sys/time.h>
+void sig(int sn);
+int main(void)
+
+{
+    while(1)
+    {
+      fflush(stdin);
+      sig(1);
+      sig(2);
+      sig(3);
+      sig(4);
+    }
+}
+void sig(int sn){
+ 
+  time_t t=time(NULL);//time() takes current time
+  printf("%d\n",sn);
+  while((time(NULL)-t)<30);//loop runs until difference b/w current time and time before loop is less than 30s
+}
+
+        
